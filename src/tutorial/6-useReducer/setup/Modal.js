e232b71d967654886e8eb6e92755240b0a73cafa
Billie Silvaner <quiinthieu@gmail.com>
2020-11-07 16:44:58 +0700

    Version 1.1.4

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,7 +1,20 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 
-const Modal = () => {
-  return <div>i'm modal</div>;
+const Modal = ({modalContent, closeModal}) => {
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            closeModal();
+        }, 3000);
+        return () => {
+            clearTimeout(timeout);
+        }
+    })
+    return (
+        <div className={"modal"}>
+            <p>{modalContent}</p>
+        </div>
+    );
 };
 
 export default Modal;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,12 +1,54 @@
-import React, { useState, useReducer } from 'react';
+import React, {useReducer, useState} from 'react';
 import Modal from './Modal';
-import { data } from '../../../data';
+import {data} from '../../../data';
+import {reducer} from "./reducer";
 
-// reducer function
-
+const defaultState = {
+    people: data,
+    showModal: false,
+    modalContent: 'Hello World'
+}
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+    const [name, setName] = useState('');
+    const [state, dispatch] = useReducer(reducer, defaultState);
+
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (name) {
+            const payload = {id: new Date().getTime().toString(), name};
+            dispatch({type: 'ADD_ITEM', payload});
+            setName('');
+        } else {
+            dispatch({type: 'NO_VALUE'});
+        }
+    }
+
+    const closeModal = () => {
+        dispatch({type: 'CLOSE_MODAL'});
+    }
+
+    return (
+        <React.Fragment>
+            {state.showModal && <Modal modalContent={state.modalContent} closeModal={closeModal}/>}
+            <form onSubmit={handleSubmit} className={"form"}>
+                <div>
+                    <input type={"text"} value={name} onChange={e => setName(e.target.value)}/>
+                </div>
+                <button type={"submit"}>Add Person</button>
+            </form>
+            {state.people.map(person => {
+                return (
+                    <div key={person.id} className={"item"}>
+                        <h4>{person.name}</h4>
+                        <button onClick={() => dispatch({type: 'REMOVE_PERSON', payload: person.id})}>Remove</button>
+                    </div>
+                )
+            })}
+        </React.Fragment>
+
+    );
 };
 
 export default Index;
